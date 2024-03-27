ee091f5e95aeac0aff267c3742f3c65747537d39
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2024-03-27 09:32:30 +0530

    add: frontend

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Quote = () => {
+  return (
+    <div className="bg-slate-200 h-screen flex justify-center items-center flex-col">
+      <div className="flex flex-col">
+        <div className="max-w-md text-3xl font-bold">
+          "The customer support I received was exceptional. The support team went
+          above and beyond my expectation"
+        </div>
+        <div className="max-w-md text-xl font-semibold mt-4">
+          Julies Winfield
+        </div>
+      </div>
+    </div>
+  );
+}; //16:00
+
+export default Quote;
---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import BlogCard from "../components/BlogCard";
+import AppBar from "../components/AppBar";
+import useBlogs from "../hooks";
+import { BlogSkeleton } from "../components/BlogSkeleton";
+
+const Blogs = () => {
+  const { loading, blogs } = useBlogs();
+
+  if (loading) {
+    return (
+      <div>
+        <AppBar />
+        <div className="flex justify-center">
+          <div>
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <AppBar />
+      <div className="flex justify-center">
+        <div>
+          {blogs.map((blog) => (
+            <BlogCard
+              authorName={blog.author.name || "Anonymous"}
+              title={blog.title}
+              content={blog.content}
+              publishedDate="2nd Feb 24"
+              id={blog.id}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blogs;
